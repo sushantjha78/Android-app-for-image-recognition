562925cedabf16307e8083a61eb0732d65e80795
Sushant Jha <[sushanthkumar78@gmail.com]>
2022-06-19 12:34:14 +0530

    Basic model and app

---
+++ app/src/main/java/com/example/cifar/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.cifar;
+
+        import androidx.annotation.Nullable;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.graphics.Bitmap;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.provider.MediaStore;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+
+        import com.example.cifar.ml.Model;
+
+        import org.tensorflow.lite.DataType;
+        import org.tensorflow.lite.support.image.TensorImage;
+        import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+        import java.io.IOException;
+        import java.nio.ByteBuffer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgView;
+    private Button select, predict;
+    private TextView tv;
+    private Bitmap img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView = (ImageView) findViewById(R.id.imageView);
+        tv = (TextView) findViewById(R.id.textView);
+        select = (Button) findViewById(R.id.button);
+        predict = (Button) findViewById(R.id.button2);
+
+
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 100);
+
+            }
+        });
+
+        predict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                img = Bitmap.createScaledBitmap(img, 32, 32, true);
+
+                try {
+                    Model model = Model.newInstance(getApplicationContext());
+
+                    // Creates inputs for reference.
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 32, 32, 3}, DataType.FLOAT32);
+                    TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
+                    tensorImage.load(img);
+                    ByteBuffer byteBuffer = tensorImage.getBuffer();
+                    inputFeature0.loadBuffer(byteBuffer);
+
+                    // Runs model inference and gets result.
+                    Model.Outputs outputs = model.process(inputFeature0);
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+                    // Releases model resources if no longer used.
+                    model.close();
+
+                    tv.setText("Cat:" + outputFeature0.getFloatArray()[3] + "\n" + "Deer:" + outputFeature0.getFloatArray()[4] + "\n"  + "Dog:" + outputFeature0.getFloatArray()[5]);
+
+                } catch (IOException e) {
+                    // TODO Handle the exception
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100)
+        {
+            imgView.setImageURI(data.getData());
+
+            Uri uri = data.getData();
+            try {
+                img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
